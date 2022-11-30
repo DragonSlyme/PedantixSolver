1e296b9cb81dfa00075ee139a887c1e06449cda2
DragonSlymeee <dragonslyme@protonmail.com>
2022-11-30 22:44:28 +0100

    Commit initial

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # PedantixSolver
-Programme simple mais efficace qui vous permet de trouver la page pédantix cachée du jour. 
+Programme __simple__ mais __efficace__ qui vous permet de trouver la page _pédantix_ cachée du jour. 
+
+---
+## Dépendances:
+Il vous faudra dans un premier temps avoir [python](https://www.python.org/downloads/) installé sur votre machine.
+
+Il vous faudra aussi __installer__ les __modules suivants__: 
+- Requests 
+- Tqdm 
+- Google
+
+Vous pouvez les installer facilement à l'aide de la commande suivante:
+```
+pip install requests tqdm google
+```
+`Cela peut prendre quelques minutes`
+
+Et enfin, il est __absolument__ nécessaire d'avoir le fichier "dictionnary.txt" dans __le même__ ficher que "pedantixSolver.py"
+
+---
+## Utilisation:
+Lancez simplement la commande suivante __après__ avoir installé les dépendances:
+```
+python pedantixSolver.py
+```
+ou alors:
+```
+python3 pedantixSolver.py
+```
+---
+## Performances:
+
+En __moyenne__ ce programme finit ces recherches entre __25 et 50 secondes__ (selon la configuration de votre pc) 
+
+`PS: Par malchance le bot peut ne pas vous renvoyer la bonne réponse`
+
+À vous de modifier le dictionnaire pour qu'il soit encore plus performant (celui-ci est d'ailleurs en partie basé sur le travail de [AnthonyJungmann](https://github.com/AnthonyJungmann/pedantix_mots/blob/master/mots_sans_stopwords.txt))
+
+Il peut largement être amélioré, c'est pourquoi je suis ouvert à toute nouvelle proposition de code.
---
+++ pedantixSolver.py
@@ -0,0 +1,124 @@
+"""
+Ce programme permet de trouver rapidement la page wikipédia pédantix du jour
+Il prend en moyenne entre 15 et 30 secondes pour proposer 3 pages wikipédia potentielles
+parmis lequels se trouve peut être la page du jour
+
+Il utilise un dictionaire simple des mots et verbes les plus courants et des méthodes
+de recherches Google afin de proposer le résultat le plus pertinent possible
+
+Pour l'utiliser, il faut avoir installé les modules requests, tqdm et google
+
+Pour installer les modules, il suffit de lancer la commande suivante dans un terminal:
+pip install requests tqdm google
+
+Pour lancer le programme, il suffit de lancer la commande suivante dans un terminal:
+python pedantixSolver.py
+
+Réalisé par @DragonSlymeee
+"""
+
+import requests
+from googlesearch import search
+from urllib.parse import unquote
+from tqdm import tqdm
+
+# Fonction pour envoyer une requête à l'API de pédantix
+def send_word(word):
+    payload = {"word": word, "answer": [word, word, word]}
+    response = requests.post(url, headers=headers,json=payload).json()
+    return response
+
+
+# Dictionnaire des mots trouver sur la page sans doublons
+def get_list_words(word, list_words):
+    response = send_word(word)
+    for words in response["score"].items():
+        word = words[1]
+        if isinstance(word, str):
+            if word.lower() not in list_words.values():
+                list_words[int(words[0])] = word.lower()
+    return list_words
+
+
+# Fonction pour tester le dictionnaire
+def test_dictionnary(dictionnary, list_words):
+    for word in tqdm(dictionnary):
+        get_list_words(word, list_words)
+    return list_words
+
+
+# Fonction pour convertir la liste des mots en une chaine de caractère utilisant les bonnes pratiques de recherches Google
+def convert_list_to_search(list_words):
+    search = ""
+    number_of_words = 0
+    for key in sorted(list_words.keys()):
+        if number_of_words < MAX:
+            search += f"\"{list_words[key]}\" "
+            number_of_words += 1
+        else:
+            break  
+    search += "site:https://fr.wikipedia.org/"
+    return search
+
+# Fonction pour récupérer les urls des pages wikipédia et n'en garder que la fin
+def get_wikipedia_url(query):
+    list_possible_words = []
+    ban_extension = [".pdf", ".html", ".fr", "/", ".htm", "#"]
+    nb_search = 10
+    nb_max_search = 3
+    for url in search(query, stop=nb_search, pause=0.1):
+        clean_word = unquote(url)[30:].replace("_", " ")
+        if nb_max_search > 0:
+            if all([extension not in clean_word for extension in ban_extension]):
+                list_possible_words.append(clean_word)
+                nb_max_search -= 1
+        else:
+            break
+    return list_possible_words
+
+# Programme principal
+def main():
+    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+    print("|            Pédantix Cracker             |")
+    print("|           by @DragonSlymeee             |")
+    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
+
+    dictionnary = open("dictionnary.txt", "r").readlines()
+    print("En recherche:")
+    list_word = test_dictionnary(dictionnary, {})
+    list_to_search = convert_list_to_search(list_word)
+    results = get_wikipedia_url(list_to_search)
+
+    i = 1
+
+    print("\nListe des mots pédantix potentiel aujourd'hui:\n")
+    for result in results:
+        print("\033[92m" + f"[+{i}+]" + "\033[0m" + f" {result}\n")
+        i += 1
+
+
+if __name__ == "__main__":
+    MAX = 31
+
+    url = "https://cemantix.certitudes.org/pedantix/score"
+
+    headers = {
+        "authority" : "cemantix.certitudes.org",
+        "method" : "POST",
+        "path" : "/pedantix/score",
+        "scheme" : "https",
+        "accept" : "*/*",
+        "accept-encoding" : "gzip, deflate, br",
+        "accept-language" : "fr-FR,fr;q=0.7",
+        "content-length" : "43",
+        "content-type" : "application/json",
+        "origin" : "https://cemantix.certitudes.org",
+        "referer" : "https://cemantix.certitudes.org/pedantix",
+        "sec-fetch-dest" : "empty",
+        "sec-fetch-mode" : "cors",
+        "sec-fetch-site" : "same-origin",
+        "sec-gpc" : "1",
+        "user-agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+    }
+
+    main()
